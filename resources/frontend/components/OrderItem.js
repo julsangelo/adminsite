1cf614cf5fc887a0dc1c396d646ce588e290e531
julsangelo <paano14325@gmail.com>
2025-07-02 19:06:45 +0800

    Update OrderItem.js

--- resources/frontend/components/OrderItem.js
+++ resources/frontend/components/OrderItem.js
@@ -25,7 +25,7 @@ export default function OrderItem({
                     <div key={index} className={styles.orderItemInfo}>
                         <div className={styles.orderItemImageContainer}>
                             <img
-                                src={`/hydrogen/${item.productImage}`}
+                                src={`${process.env.R2_PUBLIC_URL}/${item.productImage}`}
                                 alt={item.productName}
                                 className={styles.orderItemImage}
                             />
