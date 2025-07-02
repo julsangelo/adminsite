f2fd03dfb991863cf643f95ebec909f48cb06a12
julsangelo <paano14325@gmail.com>
2025-07-02 20:10:21 +0800

    Update Modal.js

--- resources/frontend/components/Modal.js
+++ resources/frontend/components/Modal.js
@@ -58,7 +58,7 @@ export default function Modal({
             case "image":
                 return (
                     <div className={styles.modalImage}>
-                        <img src={`${process.env.R2_PUBLIC_URL}/${value}`} alt="" />
+                        <img src={`${process.env.REACT_APP_R2_PUBLIC_URL}`} alt="" />
                     </div>
                 );
             case "addProductCategory":
