757bbddfe8416eb69d04e49d487841cbc1297d5e
julsangelo <paano14325@gmail.com>
2025-07-02 19:01:56 +0800

    Update Modal.js

--- resources/frontend/components/Modal.js
+++ resources/frontend/components/Modal.js
@@ -58,7 +58,7 @@ export default function Modal({
             case "image":
                 return (
                     <div className={styles.modalImage}>
-                        <img src={`https://${process.env.REACT_APP_AWS_ENDPOINT}/${process.env.REACT_APP_AWS_BUCKET}/${value}`} alt="" />
+                        <img src={`https://${process.env.AWS_ENDPOINT}/${process.env.AWS_BUCKET}/${value}`} alt="" />
                     </div>
                 );
             case "addProductCategory":
